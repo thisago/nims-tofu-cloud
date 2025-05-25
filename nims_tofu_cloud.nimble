4d8e8ce79b20162372710e816f1e6727e9424b30
thisago <thisago@disroot.org>
2025-05-25 08:01:20 -0300

    feat(app): simple auth

--- nims_tofu_cloud.nimble
+++ nims_tofu_cloud.nimble
@@ -14,3 +14,14 @@ binDir = "build"
 requires "nim >= 1.6.10"
 
 requires "jester >= 0.6.0"
+requires "dotenv >= 2.0.0"
+
+from std/strformat import fmt
+
+const hurlVariablesFile = ".env"
+
+task test, "Run Hurl tests":
+  exec fmt"hurl --variables-file {hurlVariablesFile} ./tests/*.hurl"
+
+task build_release, "Build release version":
+  exec "nimble build -d:danger --opt:speed"
--- src/nims_tofu_cloud.nim
+++ src/nims_tofu_cloud.nim
@@ -1,5 +1,20 @@
+from std/json import `%*`, `$`
+from std/os import getEnv
+
 import pkg/jester
+from dotenv import nil
+
+dotenv.load(".", ".env")
+
+let secret = getEnv "SECRET"
 
 routes:
   get "/":
-    resp "Hello, World!"
+    if request.params.getOrDefault("secret", "") == secret:
+      resp %*{
+        "message": "Hello, World!",
+      }
+    else:
+      Http401.resp $(%*{
+        "error": "Unauthorized",
+      })
