79dd1dde803d7773fb1c87ef65e1e21618fb4922
thisago <thisago@disroot.org>
2025-05-25 12:49:36 -0300

    feat(app): logging

--- src/nims_tofu_cloud.nim
+++ src/nims_tofu_cloud.nim
@@ -1,5 +1,6 @@
 from std/json import `%*`, `$`
 from std/os import getEnv, fileExists
+from std/logging import Level, addHandler, newConsoleLogger, verboseFmtStr
 
 import pkg/jester
 from pkg/dotenv import nil
@@ -8,16 +9,21 @@ from pkg/dotenv import nil
 if fileExists ".env":
   dotenv.load(".", ".env")
 
+# Initialize the logger
+addHandler(newConsoleLogger(fmtStr = verboseFmtStr))
+
 # Define the routes
 routes:
   get "/":
     # TODO: Avoid calling getEnv on every request
     let secret = getEnv "SECRET"
     if request.params.getOrDefault("secret", "") == secret:
+      logging.log(Level.lvlInfo, "Valid secret")
       resp %*{
         "message": "Hello, World!",
       }
     else:
+      logging.log(Level.lvlWarn, "Invalid secret")
       Http401.resp $(%*{
         "error": "Unauthorized",
       })
